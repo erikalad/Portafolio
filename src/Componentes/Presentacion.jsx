2bc25d94e9c5dbf9ecc25020d0e79108bcd99c56
Erika <erikaladner5@gmail.com>
2022-09-11 11:33:30 -0300

    sobremi

--- src/Componentes/Presentacion.jsx
+++ src/Componentes/Presentacion.jsx
@@ -9,12 +9,14 @@ function Presentacion () {
     return(
         <div className="cuerpo container container-lg container-md container-sm" id="Inicio">
             <img src={foto} className="foto" alt="Erika Ladner"/>
+            <div className="Inicio">
             <p className="intro"><IoRocketSharp />  BIENVENIDOS A MI PORTAFOLIO</p>
             <h1 className="titulo">ERIKA LADNER</h1>
             <h4 className="titulo">Futura Full Stack Developer</h4>
             <div className="botones">
                 <a href="https://www.linkedin.com/in/erikaladner/" className="boton"><BsLinkedin /></a>
                 <a href="https://github.com/erikalad" className="boton"><BsGithub /></a>
+            </div>
             </div>
             <SobreMi />
         </div>
--- src/Componentes/SobreMi.jsx
+++ src/Componentes/SobreMi.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import './SobreMi.css'
+import { BsFillSuitHeartFill } from "react-icons/bs";
 
 function SobreMi(){
     return(
         <div className="sobre-mi" id="sobre-mi">
-            <p>SOBRE MI</p>
-            <h1></h1>
+            <p><BsFillSuitHeartFill />  SOBRE MI</p>
+            <p className="detalle">Mi pasión es crear sitios <strong>responsivos, intuitivos y dinámicos.</strong>
+            <br />
+            ¡Te invito a conocer mi trabajo!
+            </p>
         </div>
     )
 }
