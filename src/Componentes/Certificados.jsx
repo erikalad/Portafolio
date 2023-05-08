f00b6f9bc75a3b04a522d863676ff7df114f7a5d
Erika Ladner <erikaladner5@gmail.com>
2023-05-08 14:11:43 -0300

    17

--- src/Componentes/Certificados.jsx
+++ src/Componentes/Certificados.jsx
@@ -15,6 +15,7 @@ import img13 from './../asses/13.png'
 import img14 from './../asses/14.png'
 import img15 from './../asses/15.png'
 import img16 from './../asses/16.png'
+import img17 from './../asses/17.png'
 import { GrCertificate } from 'react-icons/gr'
 import './Certificados.css'
 import Carousel from 'better-react-carousel'
@@ -76,6 +77,9 @@ export default function Certificados() {
         </Carousel.Item>
         <Carousel.Item>
         <img width="100%" src={img16}  />
+        </Carousel.Item>
+        <Carousel.Item>
+        <img width="100%" src={img17}  />
         </Carousel.Item>
        
       
